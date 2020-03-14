9f0519b52750ee5304f141ff222ffd7a65006d7e
wanghleing <1915993172@qq.com>
2020-03-14 17:48:49 +0800

    @Conditional注解过滤

--- src/main/java/com/heling/config/DistributeLockAutoConfiguration.java
+++ src/main/java/com/heling/config/DistributeLockAutoConfiguration.java
@@ -3,6 +3,10 @@ package com.heling.config;
 import com.heling.lock.DistributeLock;
 import com.heling.lock.RedisDistributeLock;
 import com.heling.lock.ZookeeperDistributeLock;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -17,15 +21,16 @@ public class DistributeLockAutoConfiguration {
 
     @Bean
     @Primary
+    @ConditionalOnMissingClass("org.apache.curator.CuratorZookeeperClient")
     public DistributeLock redisDistributeLock() {
         return new RedisDistributeLock();
     }
 
     @Bean
+    @ConditionalOnClass(name = "org.apache.curator.CuratorZookeeperClient")
     public DistributeLock zookeeperDistributeLock() {
         return new ZookeeperDistributeLock();
     }
-
 
 
 }
